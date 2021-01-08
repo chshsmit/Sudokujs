3fde6507f6d6f2c85af1a6ece6d96387a795eb11
Christopher Smith <chshsmit@gmail.com>
2021-01-08 14:48:27 -0800

    Making this application a monorepo

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -4,7 +4,7 @@
  * @description
  * @created 2020-10-20T14:39:32.323Z-07:00
  * @copyright
- * @last-modified 2021-01-05T18:44:21.983Z-08:00
+ * @last-modified 2021-01-08T14:48:03.778Z-08:00
  */
 
 import React, {
